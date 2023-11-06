9e280e441417301fde2c94d8b5b60d2a312bc4df
Platon Listkov <platon.listkov@gmail.com>
2023-11-06 18:23:33 +0300

    На проверку ментору.

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -2,8 +2,17 @@ package koschei.models;
 
 public class Egg6 {
 
+    /**
+     * Зависмость Needle7 внедряется через конструктор
+     */
+    private Needle7 needle;
+
+    public Egg6(Needle7 needle7) {
+        this.needle = needle7;
+    }
+
     @Override
     public String toString() {
-        return ", в яйце иголка " + "";
+        return ", в яйце иголка " + needle.toString();
     }
 }
